4d0e209f51331af6190af4f33f0cca75bc22b8b4
barebarebears <nick.hackland@gmail.com>
2023-02-06 16:59:31 +0000

    WIP

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>House of Plants</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <nav>
+        <img src="logo.png" alt="Logo" />
+        <ul>
+            <li>
+                <a href="">My Collection</a>
+            </li>
+            <li>
+                <a href="">Add Plant</a>
+            </li>
+            <li>
+                <a href="">Remove Plant</a>
+            </li>
+        </ul>
+    </nav>
+    <header>
+        <div>
+            <h1>Plant Tracker</h1>
+            <p>For when you have way way too many plants.</p>
+        </div>
+    </header>
+    <main>
+        <article>
+            <img alt="Picture of a Plant">
+            <div>
+                <p></p>
+                <p></p>
+                <p></p>
+            </div>
+        </article>
+    </main>
+    <footer>
+        <p></p>
+        <p></p>
+        <p></p>
+    </footer>
+</body>
+</html>
