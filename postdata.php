c4b926a41e7e15b89c8bd8a2dc1995b4d377f7c0
barebarebears <nick.hackland@gmail.com>
2023-02-09 16:18:57 +0000

    final changes from trainer review

--- postdata.php
+++ postdata.php
@@ -2,8 +2,6 @@
 
 require_once 'functions.php';
 
-session_start();
-
 //if user gets to this page without submitting correct POST data, it will send them back to index page
 if(!isset($_POST['colloquial_name']) && !isset($_POST['latin_name']) && !isset($_POST['size_cm']) && !isset($_POST['image_url'])) {
     header('Location: index.php');
@@ -17,7 +15,7 @@ $image_input = $_POST['image_url'];
 $input_value = validateNewPlant($colloquial_name_input, $latin_name_input, $size_input, $image_input);
 
 
-if ($input_value === true) {
+if ($input_value) {
     $db = createDbConnection();
     addNewPlant($db, $colloquial_name_input, $latin_name_input, $size_input, $image_input);
     header('Location: index.php');
